1e120c80466cea00f02029ffc3211dde0c640304
Juan Manuel <jm_balle@hotmail.com>
2023-02-28 19:35:44 -0300

    feat: Added context menu exercise

---
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/ContextMenuPage.java
@@ -0,0 +1,38 @@
+package com.qaprosoft.carina.demo.gui.pages;
+
+import com.qaprosoft.carina.core.foundation.webdriver.DriverHelper;
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+public class ContextMenuPage extends AbstractPage {
+    public ContextMenuPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(id = "hot-spot")
+    private ExtendedWebElement clickableBox;
+
+
+    public ExtendedWebElement getClickableBox() {
+        assertElementPresent(clickableBox);
+        return clickableBox;
+    }
+
+    public void rightClickOnBox(){
+        Actions actions = new Actions(driver);
+        actions.contextClick(getClickableBox().getElement()).perform();
+    }
+
+    //Used acceptAlert from carina instead.
+    public void acceptJsAlert(){
+        driver.switchTo().alert().accept();
+    }
+
+
+    public void getTextAlert(){
+        driver.switchTo().alert().getText();
+    }
+}
---
+++ src/test/java/com/qaprosoft/carina/demo/ContextMenuTest.java
@@ -0,0 +1,49 @@
+package com.qaprosoft.carina.demo;
+
+import com.qaprosoft.carina.core.foundation.IAbstractTest;
+import com.qaprosoft.carina.demo.gui.pages.ContextMenuPage;
+import com.zebrunner.carina.utils.Configuration;
+import com.zebrunner.carina.utils.R;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.lang.invoke.MethodHandles;
+
+public class ContextMenuTest implements IAbstractTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    ContextMenuPage contextMenuPage = null;
+
+    @BeforeSuite
+    public void startDriver() {
+        R.CONFIG.put(Configuration.Parameter.URL.getKey(), "https://the-internet.herokuapp.com/context_menu", true);
+        contextMenuPage = new ContextMenuPage(getDriver());
+    }
+
+    @Test
+    public void testOpenPage() {
+        LOGGER.info("TESTING OPENING PAGE ");
+        contextMenuPage.open();
+    }
+
+    @Test
+    public void testAlertOpening() {
+        LOGGER.info("TESTING ALERT OPENING AFTER RIGHT CLICK ");
+        contextMenuPage.open();
+        contextMenuPage.rightClickOnBox();
+        Assert.assertTrue(contextMenuPage.isAlertPresent(), "Alert did not open.");
+    }
+
+    @Test
+    public void testAcceptAlert() {
+        LOGGER.info("TESTING ALERT CLOSING AFTER ACCEPTING. ");
+        contextMenuPage.open();
+        contextMenuPage.rightClickOnBox();
+        contextMenuPage.acceptAlert();
+        Assert.assertFalse(contextMenuPage.isAlertPresent(), "Alert must not be present.");
+    }
+}
