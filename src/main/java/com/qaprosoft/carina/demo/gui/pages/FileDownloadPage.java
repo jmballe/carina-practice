ef1fa03fba05ecacda9de5abe57b4ea3210d06c1
jmballe <jm_balle@hotmail.com>
2023-03-01 02:05:17 -0300

    feat: Added file download exercise.

---
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/FileDownloadPage.java
@@ -0,0 +1,25 @@
+package com.qaprosoft.carina.demo.gui.pages;
+
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.core.gui.AbstractPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class FileDownloadPage extends AbstractPage {
+    public FileDownloadPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//div[@class=\"example\"]/a")
+    private List<ExtendedWebElement> filesLinks;
+
+    public ExtendedWebElement getFileN(int n){
+        return filesLinks.get(n);
+    }
+
+    public void clickElementN(int n){
+        getFileN(n).click();
+    }
+}
---
+++ src/test/java/com/qaprosoft/carina/demo/FileDownloadTest.java
@@ -0,0 +1,35 @@
+package com.qaprosoft.carina.demo;
+
+import com.qaprosoft.carina.core.foundation.IAbstractTest;
+import com.qaprosoft.carina.demo.gui.pages.FileDownloadPage;
+import com.zebrunner.carina.utils.Configuration;
+import com.zebrunner.carina.utils.R;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+public class FileDownloadTest implements IAbstractTest {
+    FileDownloadPage fileDownloadPage = null;
+
+    @BeforeSuite
+    public void startDriver() {
+        R.CONFIG.put(Configuration.Parameter.URL.getKey(), "https://the-internet.herokuapp.com/download"
+                , true);
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", System.getProperty("user.dir"));
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePrefs);
+        fileDownloadPage = new FileDownloadPage(getDriver("chrome",options));
+    }
+
+    @Test
+    public void testOpenPage() {
+        fileDownloadPage.open();
+        Assert.assertTrue(fileDownloadPage.isPageOpened(), "Page did not open.");
+    }
+
+}
